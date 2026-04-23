df1f0e77e91bab62122445618db68179466c00a7
TungTran <tungnghienhy@gmail.com>
2026-04-23 17:27:24 +0700

    Update MatchingMode.js

--- frontend/src/pages/MatchingMode.js
+++ frontend/src/pages/MatchingMode.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, Package, Target, Trophy } from "lucide-react";
